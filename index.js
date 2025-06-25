e43494774b2ab604d5ce85aa3fd66da13727fdef
isaiassn7 <isaiassousa19092000@gmail.com>
2025-06-25 08:52:57 -0300

    ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
   function baixarPDF() {
     const link = document.createElement("a");
-    link.href = "isaias sousa.pdf"; // Caminho do arquivo
-    link.download = "isaias sousa.pdf"; // Nome do arquivo ao baixar
+    link.href = "Isaias-Dev_Curriculo.pdf"; // Caminho do arquivo
+    link.download = "Isaias-Dev_Curriculo.pdf"; // Nome do arquivo ao baixar
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
